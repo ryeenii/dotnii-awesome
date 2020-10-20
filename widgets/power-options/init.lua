b1095b6a2ee7e62e4f19edde7ccf105b7c8df89f
ryeenii <gustarrini@pm.me>
2020-10-20 08:44:35 +0000

    move poweroff button to separate script

--- widgets/power-options/init.lua
+++ widgets/power-options/init.lua
@@ -7,10 +7,9 @@
 local awf = require('awful')
 local wib = require('wibox')
 local btf = require('beautiful')
-local uvrs = require('config.usercfg')
-local fnc = require('functions')
 local xrc = btf.xresources
 local dpi = xrc.apply_dpi
+local uvrs = require('config.usercfg')
 local grs = require('gears')
 local pwrlgo = require('widgets.power-options.logout')
 local pwroff = require('widgets.power-options.poweroff')
@@ -62,45 +61,8 @@ function pwrRev(s)
       },
       nil
     }
-    s.pwoff = wib({
-        width = 120,
-        height = 120,
-        position = 'top',
-        ontop = true,
-        visible = true,
-        border_width = dpi(2),
-        border_color = btf.bg_focus,
-        bg = btf.bg_normal,
-        shape = function(c, w, h) grs.shape.rounded_rect(c, w , h, dpi(3)) end
-    })
-    s.pwoff.fg = colorswap()
-    awf.placement.centered(s.pwoff, {margins={top = dpi(100)}})
-    s.pwotx = wib.widget {
-      markup = '⏻',
-      font = 'RecMono Nerd Font 48',
-      align = 'center',
-      widget = wib.widget.textbox
-    }
-    s.pwodi = wib.widget {
-      markup = 'poweroff',
-      font = 'RecMono Nerd Font Bold 10',
-      align = 'center',
-      widget = wib.widget.textbox
-    }
-    s.pwoff:setup {
-      layout = wib.layout.align.vertical,
-      expand = 'none',
-      nil,
-      {
-        layout = wib.layout.fixed.vertical,
-        s.pwotx
-      },
-      s.pwodi
-    }
-    s.pwoff:buttons({
-        awf.button({  }, 1, function() awf.spawn("poweroff") end)
-    })
-  end
+		poweroff(s)
+		end
   if menu then
     return
   else
---
+++ widgets/power-options/poweroff.lua
@@ -0,0 +1,47 @@
+local wib = require('wibox')
+local awf = require('awful')
+local grs = require('gears')
+local btf = require('beautiful') 
+local fnc = require('functions')
+local xrc = btf.xresources
+local dpi = xrc.apply_dpi
+function poweroff(s)
+		s.pwoff = wib({
+        width = 120,
+        height = 120,
+        position = 'top',
+        ontop = true,
+        visible = true,
+        border_width = dpi(2),
+        border_color = btf.bg_focus,
+        bg = btf.bg_normal,
+        shape = function(c, w, h) grs.shape.rounded_rect(c, w , h, dpi(3)) end
+    })
+    s.pwoff.fg = colorswap()
+    awf.placement.centered(s.pwoff, {margins={top = dpi(100)}})
+    s.pwotx = wib.widget {
+      markup = '⏻',
+      font = 'RecMono Nerd Font 48',
+      align = 'center',
+      widget = wib.widget.textbox
+    }
+    s.pwodi = wib.widget {
+      markup = 'poweroff',
+      font = 'RecMono Nerd Font Bold 10',
+      align = 'center',
+      widget = wib.widget.textbox
+    }
+    s.pwoff:setup {
+      layout = wib.layout.align.vertical,
+      expand = 'none',
+      nil,
+      {
+        layout = wib.layout.fixed.vertical,
+        s.pwotx
+      },
+      s.pwodi
+    }
+    s.pwoff:buttons({
+        awf.button({  }, 1, function() awf.spawn("poweroff") end)
+    })
+end
